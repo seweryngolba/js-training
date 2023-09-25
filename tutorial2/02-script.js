0749d14177dc1f2704c9ecd6d481a0e7385004b3
Seweryn Golba <sgoolb@gmail.com>
2023-09-25 15:42:02 +0200

    tutorial 2

---
+++ tutorial2/02-script.js
@@ -0,0 +1,11 @@
+let myVariable = "Original";
+
+let newVariable = myVariable.replace("n", "N");
+
+console.log(newVariable);
+
+console.log(Math.floor(Math.random() * 100) + 1);
+
+const anyName = "Jonathan";
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
