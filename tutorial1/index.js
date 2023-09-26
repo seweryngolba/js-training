2336d3a1b6521372edcc4004f5ada41e6f902654
Seweryn Golba <sgoolb@gmail.com>
2023-09-26 17:28:37 +0200

    tutorial loops

--- tutorial1/index.js
+++ tutorial1/index.js
@@ -1,34 +1,3 @@
-let name = "Seweryn";
-let age = 30;
-
-let person = {
-  name: "Seweryn",
-  age: 26,
-};
-
-person.age = 25;
-
-console.log(person);
-
-let selectedColors = ["red", "blue"];
-selectedColors[2] = "yellow";
-selectedColors.push("purple");
-console.log(selectedColors);
-console.log(selectedColors.length);
-
-function greet(name, lastName) {
-  console.log("Hello " + name + " " + lastName);
-}
-
-greet("John", "Johnson");
-greet("Tim", "Timon");
-
-function square(number) {
-  return number * number;
-}
-
-console.log(square(2));
-
 const messageBox = document.querySelector("#helloName");
 
 const greetMe = () => {
@@ -37,3 +6,19 @@ const greetMe = () => {
 };
 
 greetMe();
+
+let name = "Seweryn";
+for (let i = 0; i <= name.length; i++) {
+  console.log(name.charAt(i));
+}
+
+let counter = 0;
+
+while (counter <= 10) {
+  console.log(counter);
+  counter++;
+  if (counter === 2) {
+    counter++;
+    continue;
+  }
+}
