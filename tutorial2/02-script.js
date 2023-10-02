101f05eba18cdad7683c363579b6884efe5ff40d
Seweryn Golba <sgoolb@gmail.com>
2023-10-02 16:13:01 +0200

    scopes

--- tutorial2/02-script.js
+++ tutorial2/02-script.js
@@ -61,3 +61,28 @@ const toProperCase = (name) => {
 };
 
 console.log(toProperCase("tRaVis"));
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFunc() {
+  var x = 10;
+  const z = 5;
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
+  {
+    var x = 11;
+    const z = 6;
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+}
+
+myFunc();
