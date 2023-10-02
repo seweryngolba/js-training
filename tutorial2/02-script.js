4380297073880c9cf97b2cebe98e6fd0c09fa119
Seweryn Golba <sgoolb@gmail.com>
2023-10-02 15:46:47 +0200

    functions

--- tutorial2/02-script.js
+++ tutorial2/02-script.js
@@ -40,3 +40,24 @@ if (playGame) {
 } else {
   alert("Okay,maybe next time");
 }
+
+function sum(num1, num2) {
+  if (num2 === undefined) {
+    return num1 + num1;
+  }
+  return num1 + num2;
+}
+
+console.log(sum(2));
+
+function getUserNameFromEmail(email) {
+  return email.slice(0, email.indexOf("@"));
+}
+
+console.log(getUserNameFromEmail("playerOne@gmail.com"));
+
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
+console.log(toProperCase("tRaVis"));
